33b9293274f4fd6d08ad2938cf8071dc4482a0f4
Md Parvej Husen Talukder <writings.kk@gmail.com>
2025-04-04 12:23:25 +0600

    adding

--- script.js
+++
@@ -1,7 +0,0 @@
-// Function to perform search
-function search() {
-  const query = document.getElementById('search').value;
-  if (query) {
-    window.location.href = `https://www.google.com/search?q=${query}`;
-  }
-}
